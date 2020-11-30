3c72f6cd70188148c7aeba669cee1e18642220c0
James Ko <jamesqko@gmail.com>
2020-11-30 13:43:23 -0500

    Add code to scrape tweets via Selenium

--- download_tweets.py
+++ download_tweets.py
@@ -1,66 +1,89 @@
-from datetime import date, datetime, timedelta
-import functools
 import os
+import string
 import time
 
-import pandas as pd
-import tweepy
+import openpyxl
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-consumer_key = 'c2yUE9Npq9NCv7Wjucc0rn1QD'
-consumer_secret = 'lBT7lmtkEjizqGoDgtNOOCkz8BqZrlNfsoIMJzuDWCZXYIAida'
-auth = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
-api = tweepy.API(auth)
+INPUT_PATH = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'input.xlsx')
+OUTPUT_PATH = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'output.xlsx')
 
-def get_output_path(company, date_):
-    date_str = date_.strftime('%Y-%m-%d')
-    return f'data/tweets_{company}_{date_str}.csv'
+def check_load_xpath(driver, xp, timeout):
+	try:
+		e = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, xp)))
+		print('page ready')
+		return 0
+	except TimeoutError as err:
+		print('connection timed out')
+		print(err)
+		time.sleep(5)
+		return 1
 
-def gather_tweets_for_date(company, date_):
-    output_path = get_output_path(company, date_)
-    if os.path.isfile(output_path):
-        print(f"Found {company} tweets for {date_}, skipping")
-        return pd.read_csv(output_path)
+def load_excel():
+	search_requests = []
+	wb = openpyxl.load_workbook(filename=INPUT_PATH)
+	ws = wb.active
+	for cell in ws['A1':'Z1']:
+		for i in range(len(cell)):
+			if(cell[i].value):
+				search_requests.append(cell[i].value)
+	return search_requests
 
-    print(f"Gathering {company} tweets for {date_}")
+def write_excel(col, row, text):
+	if(col > 26):
+		print('EXCEL WRITE INDEX OUT OF RANGE')
+		return 1
+	if(os.path.isfile(OUTPUT_PATH) and os.access(OUTPUT_PATH, os.R_OK)):
+		print('output file exists and is readable')
+		wb = openpyxl.load_workbook(filename=OUTPUT_PATH)
+	else:
+		print('output file not present, creating new file')
+		wb = openpyxl.load_workbook(filename=INPUT_PATH)
+	ws = wb.active
+	ws[string.ascii_lowercase[col] + str(row)] = text
+	wb.save('output.xlsx')
 
-    start = datetime(year=date_.year, month=date_.month, day=date_.day)
-    end = start + timedelta(days=1)
-
-    result = api.search_full_archive(
-        environment_name='dev',
-        query=company,
-        fromDate=start.strftime('%Y%m%d%H%m'),
-        toDate=end.strftime('%Y%m%d%H%m'),
-        maxResults=30)
-    result = [status for status in result if f'${company}' in status.text.upper()]
-    # todo: handle rate limit errors
-
-    result = pd.DataFrame([{'id':, 'text':, 'timestamp':} for status in result])
-    print(result)
-    result.to_csv(output_path, index=False)
-
-    print(f"Gathered {result.shape[0]} {company} tweets for {date_}")
-    return result
 
 def main():
-    companies = sorted(['AAPL', 'AMZN', 'CHGG', 'ARW', 'UIS', 'XBIT'])
-    start_date = date(2019, 9, 29)
+	req = load_excel()
+	print(req)
+	limit = 25
+	driver = webdriver.Chrome(executable_path=r'C:\Users\james\Bin\chromedriver')
+	driver.get('https://twitter.com/explore')
+	for i in range(len(req)):
 
-    all_tweets = []
-    for i in range(140):
-        if (i % 10) == 0:
-            # For the full-archive search APIs, Twitter has a rate limit of 10 requests/sec and 30 requests/min
-            print("Napping")
-            time.sleep(20)
-            print("Waking up")
+		if(check_load_xpath(driver, '/html/body/div/div/div/div[2]/header', 10)):
+			return 1
 
-        date_ = start_date + timedelta(days=i)
-        for company in companies:
-            tweets = gather_tweets_for_date(company, date_)
-            all_tweets.extend(tweets)
+		if(i > 0):
+			driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/div/div/form/div[1]/div/div/div[2]/input').clear()
+			driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/div/div/form/div[1]/div/div/div[2]/input').send_keys(req[i])
+			driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/div/div/form/div[1]/div/div/div[2]/input').send_keys(Keys.RETURN)
+		else:
+			driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[2]/div/div/div/form/div[1]/div/div/div[2]/input').send_keys(req[i])
+			driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[2]/div/div/div/form/div[1]/div/div/div[2]/input').send_keys(Keys.RETURN)
+		time.sleep(1)
 
-    all_tweets.to_csv('data/tweets.csv', index=False)
-        
+		if(check_load_xpath(driver, '/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[2]/nav/div/div[2]/div/div[2]/a', 10)):
+			return 1
+		driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[2]/nav/div/div[2]/div/div[2]/a').click()
+		if(check_load_xpath(driver, '/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/section/div/div/div[1]/div/div/article/div/div/div/div[2]', 10)):
+			return 1
+		for j in range(1,limit):
+			if(j % 10 == 0):
+				driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+				time.sleep(3)
+			k = j - (15 * (j // 15))
+			path = '/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/section/div/div/div[' + str(k) + ']/div/div/article/div/div/div/div[2]'
+			try:
+				if(driver.find_element_by_xpath(path).text):
+					print(driver.find_element_by_xpath(path).text)
+					write_excel(i, j+1, driver.find_element_by_xpath(path).text)
+			except:
+				continue
 
-if __name__ == '__main__':
-    main()
+main()
---
+++ make_visualizations.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def main():
+    tweets_df = pd.read_csv('data/tweets.csv')
+    # TODO
+
+if __name__ == '__main__':
+    main()
