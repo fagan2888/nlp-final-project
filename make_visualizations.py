8726ef15d30a807208c3dce2986b5b0e6ed4619e
Nicholas Szczepura <nszczepura@umass.edu>
2020-12-02 16:06:19 -0500

    candlestick and subplot

--- make_visualizations.py
+++ make_visualizations.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import matplotlib.dates as mpl_dates
+from mpl_finance import candlestick_ohlc
+
 
 def main():
     companies = sorted(['AAPL', 'AMD', 'CHGG', 'ARW', 'UIS', 'XBIT'])
@@ -11,26 +14,35 @@ def main():
     #separate company data
     for i in companies:
       price_df[i] = stock_df[(stock_df['company'] == i)]
-    
-    # Create Figure and Axes instances
-    fig,ax = plt.subplots(3)
-    fig_2, ax_2 = plt.subplots(3)
 
-    for i in range(3):
-    	#plot date and close and open
-      ax[i].plot(price_df[companies[i]]['date'], price_df[companies[i]]['close'])
-      ax[i].plot(price_df[companies[i]]['date'], price_df[companies[i]]['open'])
-      ax[i].set_title(companies[i])
-      #second set
-      ax_2[i].plot(price_df[companies[i+3]]['date'], price_df[companies[i+3]]['close'])
-      ax_2[i].plot(price_df[companies[i+3]]['date'], price_df[companies[i+3]]['open'])
-      ax_2[i].set_title(companies[i+3])
-      
-      #set parameters for tick labels
-      ax[i].xaxis.set_major_locator(ticker.MultipleLocator(19))
-      ax_2[i].xaxis.set_major_locator(ticker.MultipleLocator(19))
+    plt.style.use('seaborn')
+
+    # Extracting Data for plotting
+    data = price_df['AMD']
+    ohlc = data.loc[:, ['date', 'open', 'high', 'low', 'close']]
+    ohlc['date'] = pd.to_datetime(ohlc['date'])
+    ohlc['date'] = ohlc['date'].apply(mpl_dates.date2num)
+    ohlc = ohlc.astype(float)
+
+    # Creating Subplots
+    fig, ax = plt.subplots(2, gridspec_kw={'height_ratios': [3, 1]})
+
+    candlestick_ohlc(ax[0], ohlc.values, width=0.8, colorup='darkgreen', colordown='red', alpha=0.8)
+
+    # Setting labels & titles
+    ax[0].set_xlabel('Date')
+    ax[0].set_ylabel('Price($)')
+    fig.suptitle('Daily Candlestick Chart of AMD')
+
+    # Formatting Date
+    date_format = mpl_dates.DateFormatter('%Y-%m')
+    ax[0].xaxis.set_major_formatter(date_format)
+    ax[1].xaxis.set_major_formatter(date_format)
+    fig.autofmt_xdate()
+
+    fig.tight_layout()
 
     plt.show()
-    
+
 if __name__ == '__main__':
     main()
