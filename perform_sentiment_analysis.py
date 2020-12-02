187ff8ef96121cfbde83cd88563aa9f9ca349ce3
James Ko <jamesqko@gmail.com>
2020-12-01 23:28:17 -0500

    Add annotated AMD data

--- perform_sentiment_analysis.py
+++ perform_sentiment_analysis.py
@@ -8,13 +8,14 @@ def main():
 
     for tweet in tweets_df['text']:
         result = analyzer.polarity_scores(tweet)
-        pos, neg, neu = result['pos'], result['neg'], result['neu']
-        scores.append((pos, neg, neu))
+        pos, neg, neu, comp = result['pos'], result['neg'], result['neu'], result['compound']
+        scores.append((pos, neg, neu, comp))
     
-    pos_scores, neg_scores, neu_scores = list(zip(*scores))
+    pos_scores, neg_scores, neu_scores, comp_scores = list(zip(*scores))
     tweets_df['pos'] = pos_scores
     tweets_df['neg'] = neg_scores
     tweets_df['neu'] = neu_scores
+    tweets_df['compound'] = comp_scores
 
     tweets_df.to_csv('data/tweets_anno_vader.csv', index=False)
 
