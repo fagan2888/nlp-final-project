f6e354b26d9dd3c73d9cd3c215b7e2a6a781acbb
Yefim Shneyderman <yshneyderman@umass.edu>
2020-12-04 17:06:49 -0500

    Prelim Code, Code Daily

--- prelim/code_daily.py
+++ prelim/code_daily.py
@@ -13,12 +13,12 @@ def main():
         csvreader = csv.reader(csvfile )
         for row in csvreader:
             if(count != -1):
-                sentences.append([row[0], row[1], row[3], row[4], row[5], row[6], row[7], row[8]])
+                sentences.append([row[0], row[1], row[4], row[5], row[6], row[9]])
             count += 1
         
         if(count != len(sentences)):
             print("Reading Error: Count not equal")
-    print(len(sentences))
+
     stocks = []
     count = -1
     with open('stocks.csv', encoding="utf8") as csvfile:
@@ -28,43 +28,37 @@ def main():
                 # date, company, open, close
                 stocks.append([row[0], row[1], row[6], row[3]])
             count += 1
+
     inputs = []
     outputs = []
-    pos = 0
-    neg = 0
-    neu = 0
-    total = 0
-    for s in sentences:
-        total += 1
-        if s[5] >= s[6] and s[5] >= s[7]:
-            pos += 1
-        if s[6] >= s[5] and s[6] >= s[7]:
-            neg += 1
-        if s[7] >= s[5] and s[7] >= s[6]:
-            neu += 1
-            
-    print(float(pos)/float(total))
-    print(float(neg)/float(total))
-    print(float(neu)/float(total))
+    completed = []
     for sentence in sentences:
-        company_name = str(sentence[0])
-        company_date = str(sentence[1])
-        close_price = 0
-        open_price = 0
-        flag = 0
-        for s in stocks:
-            if (s[0] == company_date and s[1] == company_name):
-                flag = 1
-                close_price = s[3]
-                open_price = s[2]
-                break
-        if (flag == 1):
-            
-            delta = float(close_price) - float(open_price)
-            if(delta >= 0):
-                count += 1
-            inputs.append([float(sentence[2]), float(sentence[3]), float(sentence[4]), float(sentence[5]), float(sentence[6]), float(sentence[7]), float(open_price)])
-            outputs.append(float(delta))
+        company_name = sentence[0]
+        company_date = sentence[1]
+        if((str(company_name) + "" + str(company_date)) not in completed):
+            close_price = 0
+            open_price = 0
+            flag = 0
+            for s in stocks:
+                if (s[0] == company_date and s[1] == company_name):
+                    flag = 1
+                    close_price = s[3]
+                    open_price = s[2]
+                    break
+            if (flag == 1):
+                completed.append(str(company_name) + "" + str(company_date))
+                delta = float(close_price) - float(open_price)
+                entry =  []
+                comp = 0
+                total = 0
+                for sen in sentences:
+                    if sen[0] == company_name and sen[1] == company_date:
+                        popularity = float(sen[2]) + float(sen[3]) + float(sen[4]) + 1
+                        total += popularity
+                        comp += popularity * float(sen[5])
+                inputs.append([comp/total, float(open_price)])
+                outputs.append(float(delta))
+
     def errorFunction(actual, predictions):
         sum = 0
         for i in range(0, len(actual)):
@@ -83,8 +77,7 @@ def main():
                 sum += 1
 
         return float(sum/len(actual))
-    
-    print(len(inputs))
+
     print("Creating sklearn Model")
     import numpy as np
     from sklearn.linear_model import LinearRegression
@@ -95,7 +88,6 @@ def main():
         actuals = outputs[start: len(inputs)]
         print("k=" + str(k) + " Percent Error: " + str(errorFunction(actuals, predictions)*100))
         print("k=" + str(k) + " Accuracy: " + str(accuracy(actuals, predictions)*100))
-        #print(reg.get_params())
 
     print("----------------------------------")
     def accuracy2(actual, predictions):
@@ -121,10 +113,10 @@ def main():
             if(predictions3[i] == 1 and actual[i] == 1):
                 oo += 1
 
-        print(float(zz))
-        print(float(oz))
-        print(float(zo))
-        print(float(oo))
+        print(float(zz)/len(actuals))
+        print(float(oz)/len(actuals))
+        print(float(zo)/len(actuals))
+        print(float(oo)/len(actuals))
 
     outputs2 = []
     for d in outputs:
@@ -132,12 +124,14 @@ def main():
             outputs2.append(0)
         elif(d >= 0):
             outputs2.append(1)
+  
     from sklearn.linear_model import LogisticRegression
     for k in [0.75, 0.8]:
         start = int(len(inputs)*k)
         reg = LogisticRegression(max_iter = 10000).fit(inputs[0:start], outputs2[0:start])
         predictions = reg.predict(inputs[start: len(inputs)])
         actuals = outputs2[start: len(inputs)]
+        print("k=" + str(k) + " Accuracy: " + str(accuracy2(actuals, predictions)*100))
 
 if __name__ == '__main__':
     main()
